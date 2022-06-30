37e099e3c511d97e94e46faa60172ed6e5a2dee8
Oleg Sliunchenko <oleg.sliuncheko@gmail.com>
2022-06-30 12:11:49 +0300

    Separate Component for apartment

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Container from './components/Container';
 import Button from './components/Button';
 import ApartmentList from './components/ApartmentList';
 import ApartmentFormComponent from './components/ApartmentFormComponent';
+import ApartmentInfComponent from "./components/ApartmentInfComponent";
 
 const mapBlockStyle = {
     width: '100%',
@@ -26,10 +27,14 @@ function App() {
         imagePath:
             'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
     };
+    const [activeApartment, setActiveApartment] = useState<ApartmentModel | undefined>(undefined);
     const {isLoaded} = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: 'AIzaSyDDsI_CW4CUgAkOIdkm6x_4z5mOZ5h1INA',
+        // @ts-ignore
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_KEY
     });
+
+
     const formSummonHandler = () => {
         setFormState(true);
     };
@@ -50,6 +55,7 @@ function App() {
     const handleActiveMarker = (id: string): void => {
         if (id !== activeMarkerId) {
             setActiveMarkerId(id);
+            setActiveApartment(apartmentList.find((item) => item.id === id));
         }
     };
 
@@ -92,58 +98,13 @@ function App() {
                             <ApartmentFormComponent submitForm={handleFormSubmit}/>
                         </Container>
                     )}
-                    {apartmentList.map<JSX.Element>((item) =>
-                        activeMarkerId === item.id ? (
-                            <>
-                                <div
-                                    key={item.id.toString()}
-                                    onClick={() => {
-                                        setMapCenter({
-                                            lat: item.location.lat(),
-                                            lng: item.location.lng(),
-                                        });
-                                    }}
-                                >
-                                    <div>
-                                        <button
-                                            className='close'
-                                            onClick={() => setActiveMarkerId(null)}
-                                        />
-                                        <img
-                                            src={require(`.${item.img_path.slice(3)}`)}
-                                            style={{width: '100%', height: '200px'}}
-                                            alt={'img'}
-                                        />
-                                        <br/>
-                                        <ul>
-                                            <li>
-                                                <p>
-                                                    <b>Title:</b> {item.title}
-                                                </p>
-                                                <p>
-                                                    <b>Address:</b>
-                                                    <br/> {item.address}
-                                                </p>
-                                                <p>
-                                                    <b>Rooms:</b> {item.rooms}
-                                                </p>
-                                                <p>
-                                                    <b>Price:</b> {item.price}
-                                                </p>
-                                                <p>
-                                                    <b>Phone Number:</b> {item.ph_number}
-                                                </p>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                <ApartmentList handler={setMapCenter}/>
-                            </>
-                        )
-                    )}
+                    {activeMarkerId !== null ?
+                        <ApartmentInfComponent
+                            apartment={activeApartment}
+                            setMapCenter = {setMapCenter}
+                            setActiveMarkerId = {setActiveMarkerId}
+                        /> : <ApartmentList handler={setMapCenter}/>
+                         }
                 </Container>
             </Container>
         </Container>
---
+++ src/components/ApartmentInfComponent.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function ApartmentInfComponent(props: any) {
+return  <div
+            onClick={() => {
+                props.setMapCenter({
+                    lat: props.apartment.location.lat(),
+                    lng: props.apartment.location.lng(),
+                });
+            }}
+        >
+            <div>
+                <button
+                    className='close'
+                    onClick={() => props.setActiveMarkerId(null)}
+                />
+                <img
+                    src={require(`../${props.apartment.img_path.slice(4)}`)}
+                    style={{width: '100%', height: '200px'}}
+                    alt={'img'}
+                />
+                <br/>
+                <ul>
+                    <li>
+                        <p>
+                            <b>Title:</b> {props.apartment.title}
+                        </p>
+                        <p>
+                            <b>Address:</b>
+                            <br/> {props.apartment.address}
+                        </p>
+                        <p>
+                            <b>Rooms:</b> {props.apartment.rooms}
+                        </p>
+                        <p>
+                            <b>Price:</b> {props.apartment.price}
+                        </p>
+                        <p>
+                            <b>Phone Number:</b> {props.apartment.ph_number}
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+}
